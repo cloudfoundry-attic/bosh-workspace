5bc6a8d971f1070e2a0fc178c44ab7b801b64945
Ruben Koster <rkoster@starkandwayne.com>
2014-01-03 22:32:13 +0100

    Added manifest validations

--- lib/bosh/manifests/manifest.rb
+++ lib/bosh/manifests/manifest.rb
@@ -1,7 +1,31 @@
 module Bosh::Manifests
   class Manifest
+    include Bosh::Cli::Validation
     def initialize(manifest_file)
+      @manifest_file = manifest_file
+    end
 
+    def perform_validation(options = {})
+      manifest_yaml = File.read(@manifest_file)
+      manifest = Psych.load(manifest_yaml)
+
+      step("Manifest file", "Manifest should be a hash", :fatal) do
+        manifest.is_a?(Hash)
+      end
+
+      step("Manifest properties", "Manifest should contain a valid name") do
+        manifest.has_key?("name") && manifest["name"].is_a?(String)
+      end
+
+      step("Manifest properties", "Manifest should contain manifests") do
+        manifest.has_key?("manifests") && manifest["manifests"].is_a?(Array)
+      end
+
+      step("Manifest properties", "Manifest should contain meta hash") do
+        manifest.has_key?("meta") && manifest["meta"].is_a?(Hash)
+      end
+
+      @manifest = manifest
     end
   end
 end
--- spec/manifest_spec.rb
+++ spec/manifest_spec.rb
@@ -1,4 +1,53 @@
 describe Bosh::Manifests::Manifest do
+  subject { Bosh::Manifests::Manifest.new manifest_file }
+  let(:manifest_file) { get_tmp_file_path(manifest) }
+  let(:name) { "foo" }
+  let(:manifests) { ["path_to_bar", "path_to_baz"] }
+  let(:meta) { { "foo" => "bar" } }
+  let(:manifest) {
+    { "name" => name, "manifests" => manifests, "meta" => meta }.to_yaml
+  }
 
+  context "invalid manifest" do
+    context "not a hash" do
+      let(:manifest) { "foo" }
+      it "raises an error" do
+        subject.validate
+        expect(subject).to_not be_valid
+        expect(subject.errors).to eq ["Manifest should be a hash"]
+      end
+    end
 
+    context "missing name" do
+      let(:manifest) { { "manifests" => manifests, "meta" => meta }.to_yaml }
+      it "raises an error" do
+        subject.validate
+        expect(subject).to_not be_valid
+        expect(subject.errors).to eq ["Manifest should contain a valid name"]
+      end
+    end
+
+    context "missing manifests" do
+      let(:manifest) { { "name" => name, "meta" => meta }.to_yaml }
+      it "raises an error" do
+        subject.validate
+        expect(subject).to_not be_valid
+        expect(subject.errors).to eq ["Manifest should contain manifests"]
+      end
+    end
+
+    context "missing meta" do
+      let(:manifest) { { "name" => name, "manifests" => manifests }.to_yaml }
+      it "raises an error" do
+        subject.validate
+        expect(subject).to_not be_valid
+        expect(subject.errors).to eq ["Manifest should contain meta hash"]
+      end
+    end
+
+  end
+
+  context "valid manifest" do
+    it { should be_valid }
+  end
 end
