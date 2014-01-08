17b1aed1226a9d94059f46ab5c62563f2766ae6b
Ruben Koster <rkoster@starkandwayne.com>
2014-01-08 15:41:50 +0100

    Simplify spiff helper

--- lib/bosh/manifests/helpers/spiff_helper.rb
+++ lib/bosh/manifests/helpers/spiff_helper.rb
@@ -1,22 +1,16 @@
 module Bosh::Manifests
   module SpiffHelper
-    def spiff
-      Bosh::Manifests::SpiffCommand.new
-    end
-  end
-
-  class SpiffCommand
     include Bosh::Exec
 
-    def merge(templates, target_file)
-      run(:merge, templates) do |output|
+    def spiff_merge(templates, target_file)
+      spiff(:merge, templates) do |output|
         File.open(target_file, 'w') { |file| file.write(output) }
       end
     end
 
     private
 
-    def run(verb, params)
+    def spiff(verb, params)
       cmd = ["spiff", verb.to_s] + params + ["2>&1"]
       sh(cmd.join(" "), :yield => :on_false) do |result|
         spiff_not_found if result.failed?
--- spec/helpers/spiff_helper_spec.rb
+++ spec/helpers/spiff_helper_spec.rb
@@ -4,40 +4,33 @@ describe Bosh::Manifests::SpiffHelper do
       include Bosh::Manifests::SpiffHelper
     end
     subject { SpiffHelperTester.new }
+    let(:path) { asset_dir("bin") }
+    let(:templates) { ["foo.yml", "bar.yml"] }
+    let(:target_file) { "spiffed_manifest.yml" }
 
-    it "creates a SpiffCommand" do
-      expect(subject.spiff).to be_instance_of(Bosh::Manifests::SpiffCommand)
+    before do
+      ENV["PATH"] = path
     end
-  end
-end
 
-describe Bosh::Manifests::SpiffCommand do
-  let(:spiff_command) { Bosh::Manifests::SpiffCommand.new }
-  subject { spiff_command }
-  let(:path) { asset_dir("bin") }
-  let(:templates) { ["foo.yml", "bar.yml"] }
-  let(:target_file) { "spiffed_manifest.yml" }
+    context "spiff not in path" do
+      let(:path) { asset_dir("empty_bin") }
+      it "raises an error" do
+        expect{ subject.spiff_merge templates, target_file }
+          .to raise_error /make sure spiff is installed/
+      end
+    end
 
-  before do
-    ENV["PATH"] = path
-  end
+    describe ".merge" do
+      let(:file) { instance_double("File") }
+      let(:output) { "---\n{}" }
+      let(:result) { Bosh::Exec::Result.new("spiff", output, 0, false) }
 
-  context "spiff not in path" do
-    subject { lambda { spiff_command.merge templates, target_file } }
-    let(:path) { asset_dir("empty_bin") }
-    it { should raise_error /make sure spiff is installed/ }
-  end
-
-  describe ".merge" do
-    let(:file) { instance_double("File") }
-    let(:output) { "---\n{}" }
-    let(:result) { Bosh::Exec::Result.new("spiff", output, 0, false) }
-
-    it "merges manifests" do
-      subject.should_receive(:sh).and_yield(result)
-      File.should_receive(:open).with(target_file, 'w').and_yield(file)
-      file.should_receive(:write).with(output)
-      subject.merge templates, target_file
+      it "merges manifests" do
+        subject.should_receive(:sh).and_yield(result)
+        File.should_receive(:open).with(target_file, 'w').and_yield(file)
+        file.should_receive(:write).with(output)
+        subject.spiff_merge templates, target_file
+      end
     end
   end
 end
