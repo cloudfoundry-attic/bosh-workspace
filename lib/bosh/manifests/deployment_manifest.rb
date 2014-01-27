9e0bc51c8cf9931c5ea1b4480dbd6dbddb043fbe
Ruben Koster <rkoster@starkandwayne.com>
2014-01-27 16:56:07 +0100

    Getting properties should work when not validated

--- lib/bosh/manifests/deployment_manifest.rb
+++ lib/bosh/manifests/deployment_manifest.rb
@@ -4,44 +4,39 @@ module Bosh::Manifests
     attr_reader :name, :director_uuid, :templates, :releases, :meta
 
     def initialize(file)
-      @file = file
+      err("Deployment file does not exist: #{file}") unless File.exist? file
+      @manifest = Psych.load(File.read(file))
     end
 
     def perform_validation(options = {})
-      manifest_yaml = File.read(@file)
-      m = Psych.load(manifest_yaml)
-
-      if m.is_a?(Hash)
-        unless m.has_key?("name") && m["name"].is_a?(String)
+      if @manifest.is_a?(Hash)
+        unless @manifest.has_key?("name") && @manifest["name"].is_a?(String)
           errors << "Manifest should contain a name"
         end
 
-        unless m.has_key?("director_uuid") && m["director_uuid"].is_a?(String)
+        unless @manifest.has_key?("director_uuid") && @manifest["director_uuid"].is_a?(String)
           errors << "Manifest should contain a director_uuid"
         end
 
-        unless m.has_key?("templates") && m["templates"].is_a?(Array)
+        unless @manifest.has_key?("templates") && @manifest["templates"].is_a?(Array)
           errors << "Manifest should contain templates"
         end
 
-        unless m.has_key?("releases") && m["releases"].is_a?(Array)
+        unless @manifest.has_key?("releases") && @manifest["releases"].is_a?(Array)
           errors << "Manifest should contain releases"
         end
 
-        unless m.has_key?("meta") && m["meta"].is_a?(Hash)
+        unless @manifest.has_key?("meta") && @manifest["meta"].is_a?(Hash)
           errors << "Manifest should contain meta hash"
         end
-        setup_manifest_attributes(m)
       else
         errors << "Manifest should be a hash"
       end
     end
 
-    private
-
-    def setup_manifest_attributes(manifest)
-      %w[name director_uuid templates releases meta].each do |var|
-        self.instance_variable_set "@#{var}", manifest[var]
+    %w[name director_uuid templates releases meta].each do |var|
+      define_method var do
+        @manifest[var]
       end
     end
   end
