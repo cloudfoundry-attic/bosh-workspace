5fa13e56c7421dd83b05ce4813b11def8b09f99c
Ruben Koster <rkoster@starkandwayne.com>
2014-05-06 14:33:40 +0200

    Remove static ips from jobs with manual networks

--- lib/bosh/workspace/helpers/dns_helper.rb
+++ lib/bosh/workspace/helpers/dns_helper.rb
@@ -2,8 +2,10 @@ module Bosh::Manifests
   class DnsHelper
     def self.transform(generated_manifest, domain_name)
       @manifest = YAML.load(IO.read(generated_manifest))
-      
+      @manual_networks = []      
+
       transform_networks
+      transform_jobs
 
       IO.write(generated_manifest, @manifest.to_yaml)
     end
@@ -13,10 +15,23 @@ module Bosh::Manifests
     def self.transform_networks
       @manifest["networks"].map! do |network| 
         if network["type"] == "manual"
+          @manual_networks << network["name"]
           { "name" => network["name"], "type" => "dynamic", "cloud_properties" => {} }
         else
           network
         end
+      end
+    end
+    
+    def self.transform_jobs
+      @manifest["jobs"].map! do |job|
+        job["networks"].map! do |network|
+          if @manual_networks.include? network["name"]
+            network.delete("static_ips")
+          end
+          network
+        end
+        job
       end
     end
   end
--- spec/helpers/dns_helper_spec.rb
+++ spec/helpers/dns_helper_spec.rb
@@ -11,8 +11,17 @@ describe Bosh::Manifests::DnsHelper do
     context "networks" do
       let(:content) { asset_file("dns/networks-openstack.yml") }
       
-      it "replaces" do
+      it "replaces manual networks" do
         expect(subject["networks"][1]["type"]).to eq "dynamic"
+      end
+    end
+
+    context "jobs" do
+      let(:content) { asset_file("dns/jobs.yml") }
+
+      it "removes static_ips of jobs with manual network" do
+        expect(subject["jobs"][0]["networks"][0]).to_not include "static_ips"
+        expect(subject["jobs"][1]["networks"][0]).to_not include "static_ips"
       end
     end
   end
