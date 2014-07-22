9109f37101f7c2e5449923363191f8db9c5fd73b
Ruben Koster <rkoster@starkandwayne.com>
2014-07-22 09:42:44 +0200

    Added stemcell prepare specs + refactor release specs

--- spec/commands/prepare_spec.rb
+++ spec/commands/prepare_spec.rb
@@ -3,48 +3,96 @@ require "bosh/cli/commands/prepare"
 describe Bosh::Cli::Command::Prepare do
   describe "#prepare" do
     let(:command) { Bosh::Cli::Command::Prepare.new }
-    let(:manifest_file) { "releases/foo-1.yml" }
-    let(:release_cmd) { instance_double("Bosh::Cli::Command::Release") }
-    let(:remote_releases) { { "versions" => remote_versions } }
-    let(:remote_versions) { [ "1" ] }
-    let(:release) do 
+    let(:release) do
       instance_double("Bosh::Workspace::Release",
         name: "foo", version: "1", repo_dir: ".releases/foo",
-        name_version: "foo/1", manifest_file: manifest_file)
+        name_version: "foo/1", manifest_file: "releases/foo-1.yml")
+    end
+    let(:stemcell) do
+      instance_double("Bosh::Workspace::Stemcell",
+        name: "bar", version: "2", name_version: "bar/2",
+        file: ".stemcesll/bar-2.tgz", file_name: "bar-2.tgz")
     end
 
     before do
-      Bosh::Cli::Command::Release.stub(:new).and_return(release_cmd)
       command.stub(:require_project_deployment)
       command.stub(:auth_required)
-      command.stub(:project_deployment_releases).and_return([release])
-      command.stub(:project_deployment_stemcells).and_return([])
-      command.stub_chain("director.get_release").and_return(remote_releases)
+      command.stub(:project_deployment_releases).and_return(releases)
+      command.stub(:project_deployment_stemcells).and_return(stemcells)
     end
 
-    context "release does not exist" do
-      it "resolves deployment requirements" do
-        command.stub_chain("director.get_release").and_raise("Release 'foo' doesn't exist`")
-        release.should_receive(:update_repo)
-        release_cmd.should_receive(:upload).with(manifest_file)
-        command.prepare
+    describe "prepare_release(s/_repos)" do
+      let(:releases) { [ release ] }
+      let(:stemcells) { [] }
+
+      before do
+        release.should_receive(:update_repo) 
+        command.should_receive(:release_uploaded?)
+          .with(release.name, release.version).and_return(release_uploaded)
+      end
+
+      context "release uploaded" do
+        let(:release_uploaded) { true }
+
+        it "does not upload the release" do
+          command.should_not_receive(:release_upload)
+          command.prepare
+        end
+      end
+
+      context "release not uploaded" do
+        let(:release_uploaded) { false }
+
+        it "does not upload the release" do
+          command.should_receive(:release_upload).with(release.manifest_file)
+          command.prepare
+        end
       end
     end
 
-    context "release version does not exist" do
-      let(:remote_versions) { [] }
-      it "resolves deployment requirements" do
-        release.should_receive(:update_repo)
-        release_cmd.should_receive(:upload).with(manifest_file)
-        command.prepare
-      end
-    end
+    describe "prepare_stemcells" do
+      let(:releases) { [] }
+      let(:stemcells) { [ stemcell ] }
 
-    context "release  exist" do
-      it "resolves deployment requirements" do
-        release.should_receive(:update_repo)
-        release_cmd.should_not_receive(:upload)
-        command.prepare
+      before do
+        command.should_receive(:stemcell_uploaded?)
+          .with(stemcell.name, stemcell.version).and_return(stemcell_uploaded)
+      end
+
+      context "stemcell uploaded" do
+        let(:stemcell_uploaded) { true }
+      
+        it "does not upload the stemcell" do
+          command.should_not_receive(:stemcell_download)
+          command.should_not_receive(:stemcell_upload)
+          command.prepare
+        end
+      end
+
+      context "stemcell not uploaded" do
+        let(:stemcell_uploaded) { false }
+
+        before { stemcell.should_receive(:downloaded?).and_return(stemcell_downloaded) }
+
+        context "stemcell downloaded" do
+          let(:stemcell_downloaded) { true }
+
+          it "does not upload the stemcell" do
+            command.should_not_receive(:stemcell_download)
+            command.should_receive(:stemcell_upload).with(stemcell.file)
+            command.prepare
+          end
+        end
+
+        context "stemcell downloaded" do
+          let(:stemcell_downloaded) { false }
+
+          it "does not upload the stemcell" do
+            command.should_receive(:stemcell_download).with(stemcell.file_name)
+            command.should_receive(:stemcell_upload).with(stemcell.file)
+            command.prepare
+          end
+        end
       end
     end
   end
